8f01c90e8ca36f3f41cf69609fdb1f423cade233
Frank Collins <fwc3rd@gmail.com>
2023-05-15 22:40:20 -0400

    fix issue #3 no TS [10:40pm]

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,9 @@
 import './App.css';
 import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom'
-import GoPage from './components/webpage/GoPage'
+// import HomeJS from './components/webpage/home'
+import HomeTS from './components/webpage/homeTS'
+
 import {connect} from "react-redux"
-import store from "./redux/store"
-
-// console.log('store')
-// console.log(store)
-
-
 
 function App() {
 
@@ -16,29 +12,10 @@ function App() {
       <Router>
 
     <Routes>
-
-    <Route path={'/'} element={ < GoPage /> } />
-
+    <Route path={'/'} element={ < HomeTS /> } />
     </Routes>
 
   </Router>
-
-    // <h1> hey whats up guys </h1>
-//     <div className="App" style={{ 
-//       backgroundImage: `url('img/noScreenPokedex.jpeg')`,
-//       display: 'flex', justifyContent: 'center', alignItems: 'center', flexDirection: 'column'
-//     }}>
-//   <div style={{  }}>
-
-// {/* firefunc firefunc2 apitest */}
-// <button style={{backgroundImage: `url('img/pokeball.png')`}}  className="Intro-Buttons Bg-Cover-noRepeat" id="IB1" onClick={firefunc}> </button>
-// <button style={{backgroundImage: `url('img/greatball.png')`}} className="Intro-Buttons Bg-Cover-noRepeat" id="IB2" onClick={firefunc2}></button>
-// <button style={{backgroundImage: `url('img/ultraball.png')`}} className="Intro-Buttons Bg-Cover-noRepeat" id="IB3" onClick={apitest}></button>
-
-//       </div>
-
-    
-//     </div>
   );
 }
 
