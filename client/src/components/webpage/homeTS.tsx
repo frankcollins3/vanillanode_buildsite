db59e73a37a5f3df2e2cf03127ba2d9f1471b857
Frank Collins <fwc3rd@gmail.com>
2023-05-15 22:42:05 -0400

    fix issue #3 and test typing of variable [10:41pm]

---
+++ client/src/components/webpage/homeTS.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function HomeTS () {
+    let myname:string = "me"
+
+    return (
+        <div>
+            <p> {myname}  </p>
+        </div>
+    )
+}
